15ceb91258d1ffb1594fd6bfe799995e7c2e0afb
victoriaachom <achom.victoria@gmail.com>
2025-03-30 14:21:13 -0700

    Added Q-Learning

--- demo.py
+++ demo.py
@@ -9,5 +9,10 @@ print("\n--- Minimax vs. Random ---")
 demoGame = prb.Game(prb.DieRollGame(), alg.MinimaxAgent(), alg.RandomAgent())
 demoGame.playGame()
 
+# Run with Q-Learning
+print("\n--- Q-Learning vs. Random ---")
+demoGame = prb.Game(prb.DieRollGame(), alg.QLearningAgent(), alg.RandomAgent())
+demoGame.playGame()
+
 #demoGame = prb.Game(prb.DieRollGame(), alg.RuleBasedAgent(), alg.RandomAgent())
 #demoGame.playGame()
